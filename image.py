bda250d83d5da1738b84562bdcb12f65d552b9fc
mfedarovich <39591973+mfedarovich@users.noreply.github.com>
2023-09-20 10:02:04 +0300

    fixed bugs

--- image.py
+++ image.py
@@ -13,10 +13,12 @@ def detect_faces(img : cv2.typing.MatLike, resolution) :
             if box !=[] and faces['confidence'] > 0.95:
                 faceBox = rectangle.make_square(box)
                 cropped_img=img[faceBox[1]: faceBox[3],faceBox[0]: faceBox[2]]
+                height, width, _ = cropped_img.shape
                 yield {
                      "image":cropped_img, 
                      "confidence" : faces['confidence'],
-                     "keypoints" : json.dumps(faces['keypoints'])
+                     "keypoints" : json.dumps(faces['keypoints']),
+                     "resolution" : f"{width}x{height}"
                      }
 
 def scale_image(image, resolution):
--- rectangle.py
+++ rectangle.py
@@ -1,22 +1,26 @@
 def make_square(box : list) :
-    maxBorder = box[2] if box[2] > box[3] else box[3]
-    faceCenter = [
+    max_border = box[2] if box[2] > box[3] else box[3]
+    face_center = [
         (int)((2*box[0]+box[2])/2), 
         (int)((2*box[1]+box[3])/2)
     ]
-    halfSize = (int)(1.3*maxBorder/2)
-    faceBox = [
-        faceCenter[0]-halfSize,
-        faceCenter[1]-halfSize,
-        faceCenter[0]+halfSize,
-        faceCenter[1]+halfSize
+    new_size = (int)(1.3*max_border)
+    shift_window_up = (int)(0.2*new_size)
+    face_center[1] = face_center[1] - shift_window_up
+    
+    half_size = (int)(new_size/2)
+    face_box = [
+        face_center[0]-half_size,
+        face_center[1]-half_size,
+        face_center[0]+half_size,
+        face_center[1]+half_size
     ]
 
-    if faceBox[0] < 0 :
-        faceBox[2] += abs(faceBox[0])
-        faceBox[0] = 0
-    if faceBox[1] < 0 :
-        faceBox[3] += abs(faceBox[1])
-        faceBox[1] = 0
+    if face_box[0] < 0 :
+        face_box[2] += abs(face_box[0])
+        face_box[0] = 0
+    if face_box[1] < 0 :
+        face_box[3] += abs(face_box[1])
+        face_box[1] = 0
     
-    return faceBox
+    return face_box
